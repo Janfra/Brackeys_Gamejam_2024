9d6fb57629af157c2e18924e2b0f0ad6ec201fe6
Jan <janfrancoherrera@gmail.com>
2025-09-14 14:40:52 +0100

    Keycodes & UI
    # Description
    Settings button currently is being used for testing. When clicked it will start listening for input and display the key and associated texture after a valid keycode is pressed. Not all textures are provided, defaulting to cursor in some.
    
    Updated the `InputActionData` to be able to retrieve from the project settings the keycodes associated with the given action. Additionally it can now verify that the selected name is registered in the `InputMap` inside the project settings.

--- Scripts/Data/InputActionData.gd
+++ Scripts/Data/InputActionData.gd
@@ -1,5 +1,38 @@
+@tool
 class_name InputActionData
 extends Resource
 
 @export 
 var action_name: StringName
+
+@export
+var keycodes: Array[Key] : get = _get_keycodes
+var _cache_keycodes: Array[Key]
+
+@export_tool_button("Verify Name And Get Keycodes")
+var _has_input = _verify_input_name
+
+func _get_keycodes() -> Array[Key]:
+	if _cache_keycodes == null:
+		return []
+	else:
+		return _cache_keycodes
+
+func _verify_input_name() -> void:
+	_cache_keycodes.clear()
+	var input_setting_name = str("input/", action_name)
+	if not ProjectSettings.has_setting(input_setting_name):
+		printerr("Unable to find action: {} in project settings. Please verify that name matches name in Input Map".format([input_setting_name], "{}"))
+	else:
+		var input_dictionary: Dictionary = ProjectSettings.get_setting(input_setting_name)
+		if input_dictionary:
+			_set_keycodes_from_settings_dictionary(input_dictionary)
+
+func _set_keycodes_from_settings_dictionary(input_dictionary: Dictionary) -> void:
+	for inputEvent in input_dictionary["events"]:
+		if inputEvent is InputEventKey:
+			if inputEvent.keycode == 0:
+				_cache_keycodes.append(inputEvent.physical_keycode as Key)
+			else:
+				_cache_keycodes.append(inputEvent.keycode as Key)
+	
--- Scripts/UI/InputActionUI.gd
+++ Scripts/UI/InputActionUI.gd
@@ -2,8 +2,19 @@ class_name InputActionUI
 extends Control
 
 @export
-var key_label: Label
+var _key_map: InputTextureMapData
+@export
+var _key_label: Label
+@export 
+var _key_texture: TextureRect
+
+func _ready() -> void:
+	assert(_key_label)
+	assert(_key_texture)
+	assert(_key_map)
+	
 
 func on_show_requested_key(requested_key: Key) -> void:
-	key_label.text = str(requested_key)
+	_key_label.text = OS.get_keycode_string(requested_key)
+	_key_texture.texture = _key_map.get_key_texture(requested_key)
 	
